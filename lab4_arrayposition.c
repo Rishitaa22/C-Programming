19b26cae49cdc2aeda970f35e9de60760a78be9e
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2025-02-05 14:11:51 +0530

    Add files via upload

---
+++ lab4_arrayposition.c
@@ -0,0 +1,29 @@
+/*
+Name: Rishita
+Date: 29/01/25
+Purpose: To find the largest among three numbers using nested if statements.
+*/
+#include<stdio.h>
+int main(){
+  int n, i, item;
+  printf("\nEnter the number of elements: ");
+  scanf("%d", &n);
+  int number[n];
+  printf("\nEnter %d elements:",n);
+  for (i=0;i<n;i++){
+    //printf(
+    scanf("%d", &number[i]);
+  }
+  printf("\nEnter element to search: ");
+  scanf("%d", &item);
+  for (i=0;i<n;i++){
+    if (item==number[i]){
+      printf("\nThe given element is found at position number:%d\n", i+1);
+    }
+    else{
+      printf("\nElement not found.");
+      break;
+    }
+  }
+  return 0;
+}
