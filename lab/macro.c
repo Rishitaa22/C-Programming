dae46ecea2b850cf1fa97bbd99d91535951e210d
Rishita <rishita.2028@cs.sjcetpalai.ac.in>
2025-03-26 14:56:58 +0530

    Add files via upload

---
+++ lab/macro.c
@@ -0,0 +1,17 @@
+/* . Using Macros: Define a macro for the value of π (PI = 3.14159) and write a program to calculate the area of a circle and a cylinder. Use macros for constant values. */
+#include<stdio.h>
+#define n 3.14
+int main(){
+  int rCircle, rCyl, hCyl;
+  float areaC, areaCyl;
+  printf("Enter the radius of circle: ");
+  scanf("%d",&rCircle);
+  printf("Enter the radius of cylinder: ");
+  scanf("%d",&rCyl);
+  printf("Enter the height of cylinder: ");
+  scanf("%d",&hCyl);
+  areaC=n*rCircle*rCircle;
+  printf("Area of Circle: %.2f\n",areaC);
+  areaCyl=2*n*rCyl*(rCyl+hCyl);
+  printf("Area of Cylinder: %.2f\n",areaCyl);
+}
